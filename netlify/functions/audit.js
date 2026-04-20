c6b455ea059090b14be3d57eced53c962d420d5b
summitstay <summitstaygroup@gmail.com>
2026-04-20 21:52:16 +0100

    Delete netlify/functions/audit.js

--- netlify/functions/audit.js
+++
@@ -1,8 +0,0 @@
-var https=require(‘https’);
-exports.handler=function(e,c,done){
-var b=JSON.parse(e.body);
-var p=JSON.stringify({model:‘claude-sonnet-4-20250514’,max_tokens:4000,messages:[{role:‘user’,content:b.prompt}]});
-var r=https.request({hostname:‘api.anthropic.com’,path:’/v1/messages’,method:‘POST’,headers:{‘Content-Type’:‘application/json’,‘x-api-key’:process.env.ANTHROPIC_KEY,‘anthropic-version’:‘2023-06-01’,‘Content-Length’:Buffer.byteLength(p)}},function(s){var d=’’;s.on(‘data’,function(c){d+=c;});s.on(‘end’,function(){done(null,{statusCode:200,headers:{‘Content-Type’:‘application/json’,‘Access-Control-Allow-Origin’:’*’},body:d});});});
-r.on(‘error’,function(err){done(null,{statusCode:500,headers:{‘Access-Control-Allow-Origin’:’*’},body:JSON.stringify({error:err.message})});});
-r.write(p);r.end();
-};
